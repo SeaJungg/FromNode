1d836416196d93ddae968f000b8b523bee2b3264
seulong <kjkj3468@naver.com>
2020-08-01 03:13:58 +0900

    second

--- main/models.py
+++ main/models.py
@@ -1,16 +1,19 @@
 from django.db import models
 
 class Members(models.Model):
+    objects = models.Manager()
     memberNo = models.IntegerField(default=0, primary_key=True, auto_created=True)
     memberName = models.CharField(max_length=125)
 
 
 class Projects(models.Model):
+    objects = models.Manager()
     projectNo = models.IntegerField(default=0, primary_key=True, auto_created=True)
     projectName = models.CharField(max_length=125)
 
 
 class Posts(models.Model):
+    objects = models.Manager()
     title = models.CharField(max_length=1023)
     date = models.DateTimeField(auto_now=True)
     previous = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank = True)
